17ebacdb74ac8c3b1d36a4f257f0a13dec752957
Ignacio Hagopian <jsign.uy@gmail.com>
2025-07-08 19:12:24 -0300

    use cargo_metadata
    Signed-off-by: Ignacio Hagopian <jsign.uy@gmail.com>

--- crates/build-utils/src/lib.rs
+++ crates/build-utils/src/lib.rs
@@ -2,19 +2,18 @@ use std::{env, fs, path::Path};
 
 // Generate a Rust source file that contains the name and version of the SDK.
 pub fn gen_name_and_sdk_version(name: &str, sdk_dep_name: &str) {
-    let workspace_dir = env::var("CARGO_WORKSPACE_DIR").unwrap();
-    let lock = fs::read_to_string(Path::new(&workspace_dir).join("Cargo.lock"))
-        .expect("Cargo.lock not found");
+    let meta = cargo_metadata::MetadataCommand::new()
+        .exec()
+        .expect("Failed to get cargo metadata");
 
-    let version = lock
-        .split("[[package]]")
-        .find(|pkg| pkg.contains(&format!("name = \"{sdk_dep_name}\"")))
-        .and_then(|pkg| {
-            pkg.lines()
-                .find(|l| l.trim_start().starts_with("version ="))
-                .and_then(|l| l.split('"').nth(1))
-        })
-        .unwrap_or_else(|| panic!("{sdk_dep_name} not found in Cargo.lock"));
+    let version = meta
+        .packages
+        .iter()
+        .find(|pkg| pkg.name.eq_ignore_ascii_case(sdk_dep_name))
+        .map(|pkg| pkg.version.to_string())
+        .unwrap_or_else(|| {
+            panic!("Dependency {sdk_dep_name} not found in Cargo.toml");
+        });
 
     let out_dir = env::var("OUT_DIR").unwrap();
     let dest = Path::new(&out_dir).join("sdk_version.rs");
